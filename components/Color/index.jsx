3d29e9afb9d90560ad76a753378f39213a048f56
Eugene Datsiuk <evgenii.datsiuk@gmail.com>
2021-04-17 14:19:07 +0300

    Added color component

---
+++ components/Color/index.jsx
@@ -0,0 +1,18 @@
+import {View} from "react-native";
+
+import React from 'react';
+
+export const Color = ({color}) => {
+    return (
+        <View
+            style={{
+                borderRadius: 50,
+                width: 100,
+                height: 100,
+                margin: 5,
+                backgroundColor: color,
+            }}
+        />
+    )
+    r
+}
